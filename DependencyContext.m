c288a1358d9ceb264abb7627ec20b0119fcde20f
Richard Noble <ranoble@gmail.com>
2011-02-18 14:26:51 +0000

    Put in the correct m file, and filled in the readme

--- DependencyContext.m
+++ DependencyContext.m
@@ -1,61 +1,54 @@
 //
-//  DependencyContext.h
+//  DependencyContext.m
 //  MacmillanCatalog
 //
 //  Created by Richard Noble on 18/11/2010.
-//  Copyright 2010 TangentLabs. All rights reserved.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
 //
 
-//Usage: 
-//
-//Like standard DI you will need to define a protocol (interface for the java minded) and one or more implementations.
-//
-//Next you will need to define a dependencies file named dependencies.plist in your resources
-//use the key column for your Service and the value as a String the implementation you wish to inject.
-//
-//include DependencyContext.h in any files that you wish to use access the dependencies for.
-//
-//From here on in you can access any service using the [[DependencyContext sharedInstance] getDependency: __SERVICESTRING__] call.
-//
-//Autowiring - can be improved: 
-//
-//Do as above; then create a property in the header file you plan to use. 
-//IE: id<Service> servicePropertyName; 
-//Next define the property further as @property(getter=[get[Service], readonly) id<Service> servicePropertyName;
-//
-//lastly in your implementation call the macro AUTO_WIRE([Service], @"[Service]") where service is the name of the service.
-//
-//now you can use the injected service as a standard (readonly) property
+#import "DependencyContext.h"
 
 
 
-#import <Foundation/Foundation.h>
-#import "SynthesizeSingleton.h"
+@implementation DependencyContext
 
-#define AUTO_WIRE(__CLASSNAME__, __SERVICESTRING__)	\
-- (id)get##__CLASSNAME__		\
-{	\
-return [[DependencyContext sharedInstance] getDependency: __SERVICESTRING__]; \
+SYNTHESIZE_SINGLETON_FOR_CLASS(DependencyContext);
+
+-(id) init{
+	if (self = [super init]){
+		NSString *filePath = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"dependencies.plist"];
+		classDefinitionDictionary = [[NSMutableDictionary alloc] initWithContentsOfFile:filePath];
+		objectDictionary = [[NSMutableDictionary alloc] initWithCapacity: classDefinitionDictionary.count];
+	}
+	return self;
 }
 
-@interface DependencyContext : NSObject {
-	//We use this to store all already instantiated object
-	NSMutableDictionary* objectDictionary;
-	
-	NSMutableDictionary* classDefinitionDictionary;
-	
+//To be overriddent for loading from alternate bundles
+-(NSBundle*) getBundle{
+	return [NSBundle mainBundle];
 }
 
-SYNTHESIZE_SINGLETON_FOR_CLASS_HEADER(DependencyContext);
+//Validations still need to be uncommented
+-(id) getDependency: (NSString*) dependencyProtocol{
+	id dep = [objectDictionary objectForKey:dependencyProtocol];
+	if (dep == nil){
+		NSString *implementationName = [classDefinitionDictionary objectForKey:dependencyProtocol];
+		//if (implementationName == nil) throw Exception
+		Class *ImplementationClass = [[self getBundle] classNamed:implementationName];
+		dep = [[ImplementationClass alloc] init];
+		//if (implementationClass != nil){}
+		//if [gribFileDataViewController conformsToProtocol:@protocol(dependencyProtocol)]
+		//NSLog([dep description]);
+		[objectDictionary setObject:dep forKey:dependencyProtocol];
+		[dep release];
+		return [objectDictionary objectForKey:dependencyProtocol];
+	}
+	return dep;
+}
 
-@property (nonatomic, retain) NSMutableDictionary* objectDictionary;
-@property (nonatomic, retain) NSMutableDictionary* classDefinitionDictionary;
-
--(id) getDependency: (NSString*) dependencyProtocol;
--(NSBundle*) getBundle;
-
-+ (DependencyContext*) sharedInstance;
-+ (void) purgeSharedInstance;
+-(void) dealloc{
+	[classDefinitionDictionary release];
+	[objectDictionary release];
+}
 
 @end
-
